abba0a436901cb87e38345af64e642b1c4478565
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-08-21 11:33:15 +0900

    update

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
- ?>
+?>
 
 <?php if (is_page('recruit')): ?>
 <?php else: ?>
@@ -18,11 +18,11 @@ $wp_url = get_template_directory_uri();
 <?php if (is_page('!recruit') ): ?>
 <section id="footer-link" class="sec">
 <div class="link-wrapper">
-  <ul>
-    <li><a href="https://kagari.ai" target="_blank"><img src="<?php echo $wp_url; ?>/lib/images/common/kagari_top.svg" alt="kagari"><p>ページのSEO/SNS流入に特化したアクセス解析ツール</p></a></li>
-    <li><a href="https://marketing.lamp.jp" target="_blank"><img src="<?php echo $wp_url; ?>/lib/images/common/lm_top.svg" alt="lamp-marketing"><p>コンテンツを強みとしたWEBマーケティング支援</p></a></li>
-    <li><a href="http://kyoto-masters.com/" target="_blank"><img src="<?php echo $wp_url; ?>/lib/images/common/km_top.svg" alt="kyotomasters"><p>京都市内の多言語観光メディアサイト</p></a></li>
-  </ul>
+<ul>
+<li><a href="https://kagari.ai" target="_blank"><img src="<?php echo $wp_url; ?>/lib/images/common/kagari_top.svg" alt="kagari"><p>ページのSEO/SNS流入に特化したアクセス解析ツール</p></a></li>
+<li><a href="https://marketing.lamp.jp" target="_blank"><img src="<?php echo $wp_url; ?>/lib/images/common/lm_top.svg" alt="lamp-marketing"><p>コンテンツを強みとしたWEBマーケティング支援</p></a></li>
+<li><a href="http://kyoto-masters.com/" target="_blank"><img src="<?php echo $wp_url; ?>/lib/images/common/km_top.svg" alt="kyotomasters"><p>京都市内の多言語観光メディアサイト</p></a></li>
+</ul>
 </div>
 </section>
 <?php endif; ?>
@@ -39,101 +39,101 @@ $wp_url = get_template_directory_uri();
 <?php if (is_home() || is_front_page() || is_page('service') ): ?>
 // slick
 $('.slick').slick({
-  autoplay: true,
-  arrows: false,
+autoplay: true,
+arrows: false,
 });
 $('.slick-logo').slick({
-  autoplay: true,
-  prevArrow: '<i class="fas fa-chevron-circle-left"></i>',
-  nextArrow: '<i class="fas fa-chevron-circle-right"></i>',
-  slidesToShow: 3,
-  arrows: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      }
-    }
-  ]
+autoplay: true,
+prevArrow: '<i class="fas fa-chevron-circle-left"></i>',
+nextArrow: '<i class="fas fa-chevron-circle-right"></i>',
+slidesToShow: 3,
+arrows: true,
+responsive: [
+{
+breakpoint: 768,
+settings: {
+slidesToShow: 2,
+slidesToScroll: 1,
+}
+}
+]
 });
 <?php endif; ?>
 
 <?php if (is_page('recruit') ): ?>
 // 採用ページのスライドショー
 $('.slider').slick({
-  infinite: true,
-  centerMode: true,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  slidesToScroll: 1,
-  dots:  true,
-  centerPadding: 'auto',
-  slidesToShow: 3,
-  responsive: [{
-    breakpoint: 770,
-    settings: {
-      centerMode: true,
-      centerPadding: 'auto',
-      slidesToShow: 1
-    }
-  }]
+infinite: true,
+centerMode: true,
+autoplay: true,
+autoplaySpeed: 3000,
+slidesToScroll: 1,
+dots:  true,
+centerPadding: 'auto',
+slidesToShow: 3,
+responsive: [{
+breakpoint: 770,
+settings: {
+centerMode: true,
+centerPadding: 'auto',
+slidesToShow: 1
+}
+}]
 });
 
 <?php endif; ?>
 // Road image
 $('.load-img').show();
 $(document).ready(function(){
-  $('.load-img').fadeOut();
+$('.load-img').fadeOut();
 });
 
 // drawer
 $(window).on('load resize', function() {
-  var w = $(window).width();
-  var x = 900;
-  $('.drawer').drawer();
+var w = $(window).width();
+var x = 900;
+$('.drawer').drawer();
 });
 
 // Scroll event
 $(window).on('load scroll', function() {
-  var s = $(window).scrollTop();
-  if (s >= 400) {
-    $('#header').addClass('scroll-on');
-    $('#header>.logo img').attr('src', '<?php echo $wp_url; ?>/lib/images/common/b_logo.svg');
-  } else {
-    $('#header').removeClass('scroll-on');
-    $('#header>.logo img').attr('src', '<?php echo $wp_url; ?>/lib/images/common/logo.svg');
-  }
+var s = $(window).scrollTop();
+if (s >= 400) {
+$('#header').addClass('scroll-on');
+$('#header>.logo img').attr('src', '<?php echo $wp_url; ?>/lib/images/common/b_logo.svg');
+} else {
+$('#header').removeClass('scroll-on');
+$('#header>.logo img').attr('src', '<?php echo $wp_url; ?>/lib/images/common/logo.svg');
+}
 });
 
 // toggle
 $('.offering-btn').on('click', function() {
-  $(this).parent().next().slideToggle();
-  $(this).toggleClass('active');
+$(this).parent().next().slideToggle();
+$(this).toggleClass('active');
 });
 $('.close').on('click', function() {
-  $(this).parent().parent().slideToggle();
+$(this).parent().parent().slideToggle();
 });
 $('#faq .dl-wrap dl dt').on('click', function() {
-  $(this).next().slideToggle();
-  $(this).toggleClass('dt-active');
+$(this).next().slideToggle();
+$(this).toggleClass('dt-active');
 });
 $('#nav ul li.parent').on('click', function() {
-  $(this).children('ul').slideToggle();
-  $(this).toggleClass('active');
+$(this).children('ul').slideToggle();
+$(this).toggleClass('active');
 });
 
 // css async road
 
 // Smooth scroll
 $('a[href^="#"]').click(function() {
-  var speed = 500;
-  var href= $(this).attr("href");
-  var target = $(href == "#" || href == "" ? 'html' : href);
-  var position = target.offset().top - 71;
-  $('body,html').animate({scrollTop:position}, speed, 'swing');
-  return false;
+var speed = 500;
+var href= $(this).attr("href");
+var target = $(href == "#" || href == "" ? 'html' : href);
+var position = target.offset().top - 71;
+$('body,html').animate({scrollTop:position}, speed, 'swing');
+return false;
 });
 
 </script>
@@ -149,33 +149,16 @@ $('a[href^="#"]').click(function() {
 <img src="<?php echo $wp_url; ?>/lib/images/common/foot_logo.svg" alt="京都のコンテンツマーケティング会社「株式会社ランプ」">
 </a>
 <ul class="mt-2">
-<!-- <li><a href="<?php echo $home; ?>/">ホーム</a></li> -->
 <li><a href="<?php echo $home; ?>/newslist">お知らせ</a></li>
 <li><a href="<?php echo $home; ?>/company/">会社紹介</a></li>
 <li><a href="<?php echo $home; ?>/service/">事業内容</a></li>
-<li><a href="<?php echo $home; ?>/portfolio/">事例紹介</a></li>
 <li><a href="<?php echo $home; ?>/recruit/">採用情報</a></li>
-<!-- <li><a href="<?php echo $home; ?>/document-request/">資料請求</a></li> -->
 <li><a href="<?php echo $home; ?>/contact/">お問い合わせ</a></li>
 </ul>
 </div>
-<!-- <ul>
-<li><a href="<?php echo $home; ?>/service/contents-marketing/">コンテンツマーケティング事業</a></li>
-<li><a href="<?php echo $home; ?>/service/">訪日インバウンド事業</a></li>
-</ul> -->
-<!-- <ul>
-<li><a href="https://blog.lamp.jp/" target="_blank" rel="noopener">conma!</a></li>
-<li><a href="http://kyoto-masters.com/" target="_blank" rel="noopener">KYOTO MASTERS</a></li>
-<li><a href="https://demo.lamp.jp/" target="_blank" rel="noopener">irori</a></li>
-<li><a href="<?php echo $home; ?>/service/">Danro</a></li>
-</ul> -->
-
 <div class="sns">
 <a href="https://www.facebook.com/KYOTO.LAMP/" target="_blank" rel="noopener"><i class="fab fa-facebook-f"></i></a>
-<a href="https://www.instagram.com/lamp.jp/" target="_blank" rel="noopener"><i class="fab fa-instagram"></i></a>
-<!-- <a href="https://lamp.jp/feed/" target="_blank" rel="noopener"><i class="fas fa-rss"></i></a> -->
 </div>
-
 </div>
 </div>
 <div class="small">
@@ -188,12 +171,5 @@ $('a[href^="#"]').click(function() {
 </footer>
 <!-- footer end -->
 <?php wp_footer(); ?>
-<!-- <script>
-(function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
-  w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
-  m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://tenzk.net/mtc.js?uid=a418485624589929f44b6768940b705da3382e0f','mt');
-mt('send', 'pageview', {"uid":"a418485624589929f44b6768940b705da3382e0f"});
-</script> -->
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -3,6 +3,14 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
 
+<section id="main-visual">
+<div class="inner">
+<p class="tlt">Creative Marketing Company <br>in Kyoto.</p>
+<h1>株式会社ランプは京都を拠点に全国の皆さまへWEBサービスをお届けする会社です。</h1>
+</div>
+<div class="scroll"><a href="#concept" class="jump">SCROLL DOWN<i class="fas fa-long-arrow-alt-down"></i></a></div>
+</section>
+
 <section id="concept">
 <div class="wrap top">
 <h2>WHAT WE DO</h2>
@@ -86,110 +94,12 @@ get_header(); ?>
 <div class="txt-inner">
 <img src="<?php echo $wp_url; ?>/lib/images/top/logo_main_kagari.png" alt="アクセス解析ツール">
 <p class="sub-ttl">アクセス解析ツール</p>
-<!-- <p>WEB担当者向けにアクセス解析レポートが自動作成できる業務効率化ツールを開発・販売しております。</p> -->
 <p>50,000ページ以上を手掛けたノウハウをもとに開発した独自のアクセス解析ツールです。</p>
 </div>
 </div>
 </a>
 </div>
-
-<!-- <div class="inner">
-<a href="https://seo.kagari.ai" target="_blank">
-<i class="fas fa-long-arrow-alt-right"></i>
-<div class="img">
-<img src="<?php echo $wp_url; ?>/lib/images/top/service_img_03.png" alt="アクセス解析ツール">
-</div>
-<div class="txt">
-<div class="txt-inner">
-<img src="<?php echo $wp_url; ?>/lib/images/top/logo_seo.png" alt="アクセス解析ツール">
-<p class="sub-ttl">アクセス解析ツール</p>
-<p>WEBサイトにあるすべてのページの検索クエリと検索順位を可視化するSEO対策ツールを開発・販売しております。</p>
-</div>
-</div>
-</a>
-</div> -->
-
-<!-- <div class="inner">
-<a href="https://kyoto-masters.com" target="_blank">
-<i class="fas fa-long-arrow-alt-right"></i>
-<div class="img">
-<img src="<?php echo $wp_url; ?>/lib/images/top/service_img_04.png" alt="">
-</div>
-<div class="txt">
-<div class="txt-inner">
-<img src="<?php echo $wp_url; ?>/lib/images/top/logo_km.png" alt="">
-<p class="sub-ttl">訪日インバウンド支援</p>
-<p>京都の宿泊施設と連携したソリューションで訪日観光客に対してのプロモーションとホスピタリティを支援いたします。</p>
-</div>
-</div>
-</a>
-</div>
-</div>
-</div> -->
-<!-- <ul class="service-list">
-<li>
-<a href="https://marketing.lamp.jp/">
-<span class="num">01</span>
-<img src="<?php echo $wp_url; ?>/lib/images/top/top_service01.png" alt="データプラットフォーム事業">
-<div class="inner">
-<h3>WEBマーケティング支援</h3>
-<p>200社以上支援した実績を持つ解析データと質の高いデジタルコンテンツを用いた企業の成長に寄り添ったWEBマーケティングを提供します。</p>
-</div>
-<i class="fas fa-long-arrow-alt-right"></i>
-</a>
-</li>
-<li>
-<a href="<?php echo $home; ?>/service/inbound-service/">
-<span class="num">02</span>
-<img src="<?php echo $wp_url; ?>/lib/images/top/top_service02.png" alt="訪日インバウンド事業">
-<div class="inner">
-<h3>訪日インバウンド支援</h3>
-<p>京都の宿泊施設と連携したソリューションで訪日観光客に対してのプロモーションとホスピタリティを支援いたします。</p>
-</div>
-<i class="fas fa-long-arrow-alt-right"></i>
-</a>
-</li>
-<li>
-<a href="<?php echo $home; ?>/service#webmarketing">
-<span class="num">03</span>
-<img src="<?php echo $wp_url; ?>/lib/images/top/top_service04.png" alt="WEBマーケティング事業">
-<div class="inner">
-<h3>アクセス解析ツール「KAGARI」</h3>
-<p>WEB担当者向けにアクセス解析レポートが自動作成・送信できる業務効率ツールやページ毎のSEO状況を可視化するツールを開発・販売しております。
-</p>
-</div>
-<i class="fas fa-long-arrow-alt-right"></i>
-</a>
-</li>
-</ul>
-<div class="logo-list slick-logo mt-5">
-<div class="kagari slick-logo-img">
-<a href="https://report.kagari.ai/" target="_blank" rel="noopener">
-<img src="<?php echo $wp_url; ?>/lib/images/common/kagarireport_top.svg" alt="ページのSEO/SNS流入に特化したアクセス解析ツール「KAGARI」">
-<p>アクセス解析レポートの<br>自動作成送信ツール</p>
-</a>
-</div>
-<div class="kagari slick-logo-img">
-<a href="https://kagari.ai/" target="_blank" rel="noopener">
-<img src="<?php echo $wp_url; ?>/lib/images/common/kagariseo_top.svg" alt="ページのSEO/SNS流入に特化したアクセス解析ツール「KAGARI」">
-<p>ページごとのSEO分析に<br>特化した解析ツール</p>
-</a>
-</div>
-<div class="lm slick-logo-img">
-<a href="https://marketing.lamp.jp/" target="_blank" rel="noopener">
-<img src="<?php echo $wp_url; ?>/lib/images/common/lm_top.svg" alt="企業のWEBマーケティングに光を灯す「LAMP Marketing」">
-<p>コンテンツを軸とした<br>WEBマーケティング支援</p>
-</a>
-</div>
-<div class="km slick-logo-img">
-<a href="http://kyoto-masters.com/" target="_blank" rel="noopener">
-<img src="<?php echo $wp_url; ?>/lib/images/common/km_top.svg" alt="訪日インバウンドメディア「KYOTO MASTRES-京都マスターズ」">
-<p>京都に特化した<br>多言語観光メディアサイト</p>
-</a>
-</div> -->
 </section>
-
-<?php get_template_part('get-portfolio'); ?>
 
 <section id="recruit">
 <div class="wrap">
