5d68f3436951a75832c3a0db65d15cbfb1fc3f7f
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-08-25 19:01:51 +0900

    update

--- front-page.php
+++ front-page.php
@@ -14,6 +14,52 @@ get_header(); ?>
 
 <div id="skip-content"></div>
 
+<section id="home-news" class="sec bg-diagonal">
+<div class="container">
+<h2 class="ttl-h2">NEWS<span>お知らせ</span></h2>
+<div class="newslist__list">
+<?php
+$args = [
+    'posts_per_page' => 3,
+    'orderby' => 'date',
+    'order' => 'DESC'
+];
+$posts = get_posts($args);
+foreach ($posts as $post): setup_postdata($post);
+$t = get_the_title();
+$p = get_the_permalink();
+$categories = get_the_category();
+if (has_post_thumbnail()) {
+    $i = get_the_post_thumbnail_url(get_the_ID(), 'large');
+} else {
+    $i = $wp_url.'/dist/images/no-img.png';
+}
+?>
+<a class="newslist__list-item" href="<?php echo $p; ?>">
+<div class="newslist__list-thumbnail">
+<img src="<?php echo $i; ?>" alt="<?php echo $t; ?>">
+</div>
+<div class="newslist__list-txt">
+<p class="newslist__list-ttl"><?php echo $t; ?></p>
+<div class="newslist__list-meta">
+<span><?php the_time('Y.m.d'); ?></span>
+<span><?php echo $categories[0]->name; ?></span>
+</div>
+</div>
+</a>
+<?php endforeach; wp_reset_postdata(); ?>
+</div>
+<div class="more-btn">
+<a href="<?php echo $home; ?>/newslist/">VIEW MORE<i class="fas fa-long-arrow-alt-right ml-3"></i></a>
+</div>
+</div>
+</section>
+
+<section id="home-service" class="sec">
+  <div class="container">
+  </div>
+</section>
+
 <section id="recruit" class="sec">
 <div class="container">
 <h2 class="ttl-h2">RECRUIT<span>採用について</span></h2>
