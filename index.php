70693abed0a749fbb67ec3e38a95871d2513fe33
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-08-27 15:56:57 +0900

    update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
+$now_slug = str_replace('/', '', $_SERVER['REQUEST_URI']);
 get_header(); ?>
 
 <section id="sub-visual" <?php echo $thumbnail; ?>>
@@ -12,28 +13,55 @@ get_header(); ?>
 </div>
 </section>
 
-<section class="sec newslist">
+<section class="sec">
 <div class="container">
-<ul class="newslist__cats">
-<li><a href="<?php echo $home; ?>/newslist/"><i class="fas fa-tag text-light mr-2"></i>すべて</a></li>
-<li><a href="<?php echo $home; ?>/news/"><i class="fas fa-tag text-warning mr-2"></i>お知らせ</a></li>
-<li><a href="<?php echo $home; ?>/press-release/"><i class="fas fa-tag text-danger mr-2"></i>プレスリリース</a></li>
-<li><a href="<?php echo $home; ?>/event/"><i class="fas fa-tag text-success mr-2"></i>イベント</a></li>
-</ul>
 
-<div class="newslist-list">
+<div class="newslist__cats">
+<?php
+if ($now_slug === 'newslist') {
+  $cat_class = 'active';
+} else {
+  $cat_class = '';
+}
+?>
+<a class="<?php echo $cat_class; ?>" href="<?php echo $home; ?>/newslist/">すべて</a>
+<?php
+$cats = get_categories();
+foreach ($cats as $key => $cat):
+if ($now_slug === $cat->slug) {
+    $cat_class = 'active';
+} else {
+    $cat_class = '';
+}
+?>
+<a class="<?php echo $cat_class; ?>" href="<?php echo $home.'/'.$cat->slug; ?>"><?php echo $cat->name; ?></a>
+<?php endforeach; ?>
+</div>
+
+<div class="newslist__list">
 <?php
 if (have_posts()): while (have_posts()):
 the_post();
 $t = get_the_title();
 $p = get_the_permalink();
-$cat_name = get_the_category()[0]->name;
-$cat_slug = get_the_category()[0]->slug;
+$categories = get_the_category();
+if (has_post_thumbnail()) {
+    $i = get_the_post_thumbnail_url(get_the_ID(), 'large');
+} else {
+    $i = $wp_url.'/dist/images/no-img.png';
+}
 ?>
-<a class="<?php echo $cat_slug; ?>" href="<?php echo $p; ?>">
-<span class="time"><?php the_time('Y.m.d'); ?></span>
-<span class="cat"><?php echo $cat_name; ?></span>
-<span class="ttl"><?php echo $t; ?></span>
+<a class="newslist__list-item" href="<?php echo $p; ?>">
+<div class="newslist__list-thumbnail">
+<img src="<?php echo $i; ?>" alt="<?php echo $t; ?>">
+</div>
+<div class="newslist__list-txt">
+<p class="newslist__list-ttl"><?php echo $t; ?></p>
+<div class="newslist__list-meta">
+<span><?php the_time('Y.m.d'); ?></span>
+<span><?php echo $categories[0]->name; ?></span>
+</div>
+</div>
 </a>
 <?php endwhile; endif; ?>
 </div>
