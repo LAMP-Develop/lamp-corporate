ffc71959de401dd4fe1da3611c536198806094ea
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-08-31 18:53:11 +0900

    update

--- header.php
+++ header.php
@@ -7,8 +7,17 @@ $wp_url = get_template_directory_uri();
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap">
 <?php wp_head(); ?>
 <?php if (!is_user_logged_in()): ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79537153-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-79537153-2');
+</script>
 <?php endif; ?>
 </head>
 <body <?php body_class(); ?> data-tmpdir="<?php echo $wp_url; ?>">
